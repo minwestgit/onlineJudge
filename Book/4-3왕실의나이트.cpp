efda688aa419a2a52f33e7f0952e3113ffc6009b
minwestgit <minwest61@gmail.com>
2021-06-22 22:54:49 +0900

    수정

--- Book/4-3왕실의나이트.cpp
+++ Book/4-3왕실의나이트.cpp
@@ -4,63 +4,23 @@
 
 using namespace std;
 
-int n, m, x, y, dir;
-int d[50][50]; // 방문여부 기록
-int arr[50][50];
 
-// 북,동,남,서
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-
+int dx[] = {-2, -2, 2, 2, -1, -1, 1, 1};
+int dy[] = {-1, 1, -1, 1, -2, 2, -2, 2};
+ 
 int main(int argc, const char * argv[]) {
-    cin >> n >> m;
-    cin >> x >> y >> dir;
+    string str;
+    cin >> str;
+    int x = str[1] - '0';
+    int y = str[0] - 'a' + 1;
 
-    memset(d, 0, sizeof(d)); // d 초기화
-    d[x][y] = 1; // 시작 위치 방문
+    int cnt = 0;
+    for(int i=0;i<8;i++) {
+        int nx = x+dx[i];
+        int ny = y+dy[i];
 
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++) {
-            cin >> arr[i][j];
-        }
-    }
-
-    int cnt = 1;
-    int turnCnt = 0;
-    while(1) {
-
-        // 1단계 : 왼쪽 방향으로 회전
-        dir -= 1;
-        if(dir == -1) dir = 3;
-
-        // 2단계 : 가보지 않은 칸이 존재하는 경우 이동
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        if(arr[nx][ny] != 1 && d[nx][ny] == 0) {
-            x = nx;
-            y = ny;
-            d[x][y] = 1;
+        if(nx>0 && nx<=8 && ny>0 && ny<=8) {
             cnt++;
-            turnCnt = 0;
-            continue;
-        } 
-        // 가보지 않은 칸이 없거나 바다인 경우 1단계로 돌아감
-        else {
-            turnCnt++;
-        }
-
-        // 3단계 : 네 방향 모두 갈 수 없는 경우
-        if(turnCnt == 4) {
-            nx = x - dx[dir];
-            ny = y - dy[dir];
-            // 뒤로 한 칸 이동
-            if(arr[nx][ny] == 0) {
-                x = nx;
-                y = ny;
-                turnCnt = 0;
-                break;
-            }
-            else break; // 바다이면 멈춤
         }
     }
 
