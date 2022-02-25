538d310d1296854e6465427121c4e40c5ff14154
minwestgit <minwest61@gmail.com>
2022-02-25 21:21:58 +0900

    lv2

--- Programmers/다리를지나는트럭.java
+++ Programmers/다리를지나는트럭.java
@@ -4,22 +4,32 @@ class Solution {
         int answer = 0;
         int sum = 0, i = 0;
         Queue<Integer> q = new LinkedList<>();
-        while(true) {
-            answer += 1;
-            if(q.size() == bridge_length) {
-                sum -= q.poll();
-            }
-            if(sum + truck_weights[i] <= weight) {
-                if(i == truck_weights.length-1) {
-                    return answer+bridge_length;
+        for(int truck : truck_weights) {
+            while(true) { // 트럭 올릴 때 까지 반복
+                answer += 1;
+                // 큐가 비어있으면 다리에 트럭 올림.
+                if(q.isEmpty()) {
+                    q.add(truck);
+                    sum += truck;
+                    break;
                 }
-                q.add(truck_weights[i]);
-                sum += truck_weights[i];
-                i++;
-            }
-            else {
-                q.add(0);
+                // 다리에 트럭이 최대로 올라가면 큐에서 제거
+                if(q.size() == bridge_length) {
+                    sum -= q.poll();
+                }
+                // 다리에 트럭 더 올릴 수 있으면 큐에 추가
+                if(sum + truck <= weight) {
+                    q.add(truck);
+                    sum += truck;
+                    break;
+                }
+                // 못 올리면 0 추가
+                else {
+                    q.add(0);
+                }
             }
         }
+        
+        return answer + bridge_length;
     }
 }
