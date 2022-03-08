6fcad86a53f0dfe59299bf17b34d6446e99a0ce2
minwestgit <minwest61@gmail.com>
2022-03-08 18:18:34 +0900

    복구

--- Codility/SocksLaundering.java
+++ Codility/SocksLaundering.java
@@ -1,42 +1,44 @@
 import java.util.*;
 class Solution {
     public int solution(int K, int[] C, int[] D) {
-        // write your code in Java SE 8
         int answer = 0;
-        int[] clean = new int[51];
-        int[] dirty = new int[51];
+        Arrays.sort(C);
+        Arrays.sort(D); // binarySearch하기 위해 정렬
 
-        // 깨끗한 양말 번호별로 count해 저장
-        for(int c : C) {
-            clean[c] += 1;
-        }
-        // 더러운 양말 번호별로 count해 저장
-        for(int d : D) {
-            dirty[d] += 1;
-        }
-
-        for(int i = 1; i < 51; i++) {
-            answer += clean[i]/2; // 깨끗한 양말끼리 가능한 쌍 계산
-            // 깨끗한 양말 + 더러운 양말 조합 계산
-            if(clean[i]%2 != 0 && K > 0 && dirty[i] > 0) {
+        for(int i = 0; i < C.length; i++) {
+        	// 깨끗한 양말끼리 챙겨갈 수 있는 경우
+            if(i != C.length-1 && C[i] == C[i+1]) {
+                i += 1; // i+1도 이미 비교한 셈이므로 +1
                 answer += 1;
-                K -= 1;
-                dirty[i] -= 1;
             }
+            // Clean + Dirty 가능한지 체크
+            else {
+                int idx = Arrays.binarySearch(D, C[i]);
+                // Dirty에 해당 번호의 양말(Clean)이 존재하면 챙김
+                if(idx >= 0 && K > 0) {
+                    K -= 1;
+                    answer += 1;
+                    D[idx] = 0; // 다시 못 빨게 0으로 바꿔줌
+                }
+            } 
         }
-
-        // 세탁 가능한 양말이 아직 남아있다면 더러운 양말 2개씩 묶어 처리
-        for(int i = 1; i < 51; i++) {
-            if(K < 2) break; // 세탁가능한 양말 개수가 2개 이상 남아있지 않다면 break
-            // i번 더러운 양말이 2개 이상 남아있으면 세탁
-            if(dirty[i] >= 2) {
-                // i번 양말 쌍보다 세탁 가능한 양말 개수 K 중 더 작은 수 만큼만 세탁 가능
-                int wash = Math.min(dirty[i], K)/2;
-                answer += wash;
-                K -= 2*wash;
+        
+        Arrays.sort(D); // 더러운 양말끼리 묶기 위해 정렬
+        
+        // 세탁 가능한 양말이 아직 남아있다면 더러운 양말끼리 세탁해 챙길 수 있는지 체크
+        for (int i = 0; i < D.length-1; i++) {
+        	// 세탁 가능한 양말이 2개 미만으로 남아있다면 break
+            if(K < 2){
+				break;
+			}
+            // 더러운 양말 1쌍이 존재한다면
+            if(D[i] == D[i+1] && D[i] != 0){
+				answer += 1;
+				K -= 2;
+				i += 1;
             }
-        }
 
+		}
         return answer;
-    }
-}
+    }   
+}  
