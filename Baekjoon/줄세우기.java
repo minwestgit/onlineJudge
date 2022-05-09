05a5ab589941f5a369380403f9683307a90b8522
minwestgit <minwest61@gmail.com>
2022-05-09 20:20:29 +0900

    2605

---
+++ Baekjoon/줄세우기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class 줄세우기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            list.add(i-num, i+1);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
