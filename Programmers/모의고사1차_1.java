8c35cf9cdd9c25938ded0f545b3d3037d8d79fa3
minwestgit <minwest61@gmail.com>
2022-08-06 23:59:24 +0900

    실전모의고사 1차 문제집_1번

---
+++ Programmers/모의고사1차_1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public String solution(String X, String Y) {
+        String answer = "";
+
+        StringBuilder result = new StringBuilder();
+        String[] xx = X.split("");
+        String[] yy = Y.split("");
+        Arrays.sort(xx, Collections.reverseOrder()); Arrays.sort(yy, Collections.reverseOrder());
+        int xi = 0, yi = 0;
+        while(xi < xx.length && yi < yy.length) {
+            if(xx[xi].equals(yy[yi])) {
+                result.append(xx[xi]);
+                xi += 1;
+                yi += 1;
+            }
+            else {
+                if(Integer.parseInt(xx[xi]) > Integer.parseInt(yy[yi])) xi += 1;
+                else yi += 1;
+            }
+        }
+        answer = result.toString();
+        if(answer.equals("")) return "-1";
+        if(answer.charAt(0) == '0') return "0";
+
+        return answer;
+    }
+}
