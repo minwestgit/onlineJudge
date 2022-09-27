3e8d672b4ff4f54e161a55bec0acc0517347af30
minwestgit <minwest61@gmail.com>
2022-09-28 00:05:19 +0900

    방법2

--- Programmers/최고의집합.java
+++ Programmers/최고의집합.java
@@ -17,3 +17,21 @@ class Solution {
         return answer;
     }
 }
+
+class Solution2 {
+    public int[] solution(int n, int s) {
+        int[] answer = new int[n];
+        int num = s/n;
+        if(num == 0) return new int[] {-1};
+        
+        Arrays.fill(answer, num);
+        int rem = s%n; //num으로 배열채우고 남은 나머지
+        int i = n-1;
+        while(rem > 0) { //rem값 answer배열에 값 하나씩 +1
+            answer[i] += 1;
+            rem -= 1;
+            i -= 1;
+        }
+        return answer;
+    }
+}
