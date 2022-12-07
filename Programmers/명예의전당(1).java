61f893e0e6880f206a40cbfec72fd13c50781f70
minwestgit <minwest61@gmail.com>
2022-12-07 22:16:41 +0900

    코드 업그레이드~

--- Programmers/명예의전당(1).java
+++ Programmers/명예의전당(1).java
@@ -2,12 +2,14 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         List<Integer> ans = new ArrayList<>();
-        List<Integer> tops = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int s : score) {
-            tops.add(s);
-            Collections.sort(tops, Collections.reverseOrder());
-            if(tops.size() < k) ans.add(tops.get(tops.size()-1));
-            else ans.add(tops.get(k-1));
+            pq.add(s);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            ans.add(pq.peek());
+
         }
         return ans.stream()
                 .mapToInt(i -> i)
