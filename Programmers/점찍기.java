d98d26eb7016c682ae7ac0001af27926d7a714aa
minwestgit <minwest61@gmail.com>
2023-02-02 13:41:24 +0900

    개선

--- Programmers/점찍기.java
+++ Programmers/점찍기.java
@@ -1,11 +1,9 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        for (long a = 0; a <= d/k; a++) {
-            long x = a*k;
-            long y = (long) Math.sqrt((long) Math.pow(d, 2) - (long) Math.pow(x, 2));
-
-            answer += (y/k)+1;
+        for (long a = 0; a<=d; a+=k) {
+            long y = (long) Math.sqrt((long)d*(long)d - a*a); //x^2+y^2<=d^2
+            answer += (int) (y/k)+1; //0까지 포함해주기 위해서 +1
         }
         return answer;
     }
