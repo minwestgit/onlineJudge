1fc7feedf47e426ea8085f8f8680e69231a9369b
minwestgit <minwest61@gmail.com>
2023-02-06 23:10:51 +0900

    수정

--- Programmers/둘만의암호.java
+++ Programmers/둘만의암호.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        for(int i = 0; i < s.length(); i++) {
-            char n = s.charAt(i);
-            for(int j = 1; j <= index; j++) {
-                n += 1;
-                if(n > 'z') n -= 26;
-                if(skip.contains(String.valueOf(n))) j -= 1;
+        for(char c : s.toCharArray()) {
+            for(int j = 0; j < index; j++) {
+                c += 1;
+                if(c > 'z') c -= 26;
+                if(skip.contains(String.valueOf(c))) j -= 1;
             }
-            answer += String.valueOf(n);
+            answer += String.valueOf(c);
         }
         return answer;
     }
